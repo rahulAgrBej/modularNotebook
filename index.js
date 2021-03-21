79e82940610f007a21c2770fe10688d30f1ddd00
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-03-21 16:37:01 -0400

    works now with a production mode using build from front end

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 var cors = require('cors');
 let app = express(); 
 let port = process.env.PORT || 8080;
-
+var path = require('path');
 app.use(cors());
+
+if(process.env.NODE_ENV === 'production'){
+  // If running in production mode enter here
+	app.use(express.static(path.join(__dirname, 'frontend/build')));
+	app.get('/', function(req,res) {
+		res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+	});
+}
+
 app.get('/hello', function(req, res) { 
     res.json('Hello from backend !'); 
 })
