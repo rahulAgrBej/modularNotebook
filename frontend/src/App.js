96a76208d8cdf0c748f2fd30fccea91e1a686e3e
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-03-21 16:28:39 -0400

    working with a proxy for front end, package json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [message, setMessage] = useState('');
   // Send a request to the server on port 8080 to retrieve message
   useEffect(() => {
-    fetch('http://localhost:8080/hello')
+    fetch('/hello')
 			.then(response => {
 				return response.json();
 			})
